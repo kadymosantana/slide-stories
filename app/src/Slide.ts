5b94c1bbe2d4fd587bf99c9c1fcce27450cd3ef0
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2022-11-30 02:44:25 +0000

    feat: "show" and "hide" Methods Added To Class

--- app/src/Slide.ts
+++ app/src/Slide.ts
@@ -1,19 +1,33 @@
 export default class Slide {
   container;
-  elements;
+  slides;
   controls;
   time;
+  index: number; // index that is currently active
+  slide: Element; // slide that is currently active
 
   constructor(
     container: Element,
-    elements: Element[],
+    slides: Element[],
     controls: Element,
     time: number = 5000) {
       this.container = container;
-      this.elements = elements;
+      this.slides = slides;
       this.controls = controls;
-      this.time = time
-    
-      console.log(this.container)
+      this.time = time;
+
+      this.index = 0;
+      this.slide = this.slides[this.index]
     }
+
+  show(index: number) {
+    this.index = index;
+    this.slide = this.slides[index];
+    this.slides.forEach(el => this.hide(el));
+    this.slide.classList.add("active");
+  }
+
+  hide(el: Element) {
+     el.classList.remove("active");
+  }
 }

ea347c646b804c16da837b458adde96625bf80e8
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2022-12-03 01:12:08 +0000

    Added Pause Method

--- app/src/Slide.ts
+++ app/src/Slide.ts
@@ -8,62 +8,89 @@ export default class Slide {
   index: number; // index that is currently active
   slide: Element; // slide that is currently active
   timeout: Timeout | null;
+  paused: boolean;
+  pausedTimeout: Timeout | null;
 
   constructor(
     container: Element,
     slides: Element[],
     controls: Element,
-    time: number = 5000) {
-      this.container = container;
-      this.slides = slides;
-      this.controls = controls;
-      this.time = time;
+    time: number = 5000
+  ) {
+    this.container = container;
+    this.slides = slides;
+    this.controls = controls;
+    this.time = time;
 
-      this.timeout = null
-      this.index = 0;
-      this.slide = this.slides[this.index]
-    }
+    this.timeout = null;
+    this.index = 0;
+    this.slide = this.slides[this.index];
+
+    this.paused = false;
+    this.pausedTimeout = null;
+
+    this.init();
+  }
 
   show(index: number) {
     this.index = index;
     this.slide = this.slides[index];
-    this.slides.forEach(el => this.hide(el));
+    this.slides.forEach((el) => this.hide(el));
     this.slide.classList.add("active");
-    this.auto(this.time)
+    this.auto(this.time);
   }
 
   hide(el: Element) {
-     el.classList.remove("active");
+    el.classList.remove("active");
   }
 
   auto(time: number) {
-    this.timeout?.clear()
-    this.timeout = new Timeout(() => this.next(), time)
+    this.timeout?.clear();
+    this.timeout = new Timeout(() => this.next(), time);
   }
 
   prev() {
-    if(this.index > 0) this.show(this.index - 1)
+    if (this.paused) return;
+    if (this.index > 0) this.show(this.index - 1);
   }
 
   next() {
-    this.index < this.slides.length - 1 ?
-    this.show(this.index + 1) :
-    this.show(0)
+    if (this.paused) return;
+    this.index < this.slides.length - 1
+      ? this.show(this.index + 1)
+      : this.show(0);
+  }
+
+  pause() {
+    this.pausedTimeout = new Timeout(() => {
+      this.paused = true;
+    }, 1000);
+  }
+
+  continue() {
+    this.pausedTimeout?.clear();
+    if (this.paused) {
+      this.paused = false;
+      this.auto(this.time);
+    }
   }
 
   private addControls() {
-    const prevButton = document.createElement("button")
-    const nextButton = document.createElement("button")
+    const prevButton = document.createElement("button");
+    const nextButton = document.createElement("button");
 
-    this.controls.appendChild(prevButton)
-    this.controls.appendChild(nextButton)
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
 
-    prevButton.addEventListener("pointerup", () => this.prev())
-    nextButton.addEventListener("pointerup", () => this.next())
+    this.controls.addEventListener("pointerdown", this.pause);
+    this.controls.addEventListener("pointerup", this.continue);
+
+    prevButton.addEventListener("pointerup", () => this.prev());
+    nextButton.addEventListener("pointerup", () => this.next());
   }
 
   private init() {
-    this.addControls()
-    this.show(this.index)
+    this.addControls();
+    this.show(this.index);
   }
 }
